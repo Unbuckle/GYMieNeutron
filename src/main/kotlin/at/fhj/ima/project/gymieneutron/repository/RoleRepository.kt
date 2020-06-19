4b2d6edb9a88f5a8442c7e6d158cc0e1651234aa
karimres17 <reshan.karim@edu.fh-joanneum.at>
2020-06-19 11:38:57 +0200

    new jsps 2.0

--- src/main/kotlin/at/fhj/ima/project/gymieneutron/repository/RoleRepository.kt
+++
@@ -1,8 +0,0 @@
-package at.fhj.ima.project.gymieneutron.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface RoleRepository : JpaRepository<Role, Int> {
-}
